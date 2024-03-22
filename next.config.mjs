95018ea375650b78f25372aa7d1372dd430a9c5b
ihorSamolich <126615259+ihorSamolich@users.noreply.github.com>
2024-03-22 14:29:37 +0200

    First commit

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,28 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+    reactStrictMode: false,
+    images:{
+        remotePatterns:[
+            {
+                protocol: 'https',
+                hostname: "tailwindui.com"
+            },
+            {
+                protocol: 'http',
+                hostname: "localhost",
+                port: "5170"
+            },
+            {
+                protocol: 'https',
+                hostname: "blog.ihor.fun",
+            }
+        ]
+    },
+    experimental: {
+        serverActions: {
+            bodySizeLimit: "20mb",
+        },
+    },
+}
 
 export default nextConfig;
